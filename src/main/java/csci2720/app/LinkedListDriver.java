0f809f002f7f67c4aff35a17591cf9ad33affaf0
Isaias Pavon <ip13488@uga.edu>
2024-09-16 15:19:12 -0400

    Maven and initial files created

---
+++ src/main/java/csci2720/app/LinkedListDriver.java
@@ -0,0 +1,45 @@
+package csci2720.app;
+
+public class LinkedListDriver {
+
+    public static void main(String[] args) {
+
+        boolean quit = false;
+
+        switch(!quit) {
+        case 'i':
+            System.out.println("");
+            continue;
+        case 'd':
+            System.out.println("");
+            continue;
+        case 's':
+            System.out.println("");
+            continue;
+
+        case 'a':
+            System.out.println("");
+            continue;
+        case 'm':
+            System.out.println("");
+            continue;
+        case 't':
+            System.out.println("");
+            continue;
+        case 'p':
+            System.out.println("");
+            continue;
+        case 'l':
+            System.out.println("");
+            continue;
+        case'q':
+            System.out.println("");
+            break;
+        default:
+            System.out.println("Invalid command, try again!");
+        } // switch
+
+
+    } // main
+
+} // LinkedListDriver
